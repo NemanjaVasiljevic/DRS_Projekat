8a8f5859c190c690cf12009e605fac90d5023a5b
Gvoz29 <branislavgvozdenov29@gmail.com>
2022-12-28 19:26:05 +0100

    Nesto sam zeznuo

--- UI/API/__init__.py
+++
@@ -1,6 +0,0 @@
-from flask import Flask
-
-app = Flask(__name__)
-app.config['SECRET_KEY'] = 'ec9439cfc6c796ae2029594d'
-
-from API import routes
--- UI/run.py
+++
@@ -1,6 +0,0 @@
-from API import app
-import os
-
-if __name__ == '__main__':
-    port=int(os.environ.get('PORT',5001))
-    app.run(debug=True,port=port,host='0.0.0.0')
